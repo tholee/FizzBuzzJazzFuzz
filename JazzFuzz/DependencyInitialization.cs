774b9c661abcb224d3f27d52cdb722b338ee458d
Tomas Hensrud Gulla <tomas.h.gulla@novacare.no>
2019-03-28 20:50:49 +0100

    Change defaults

--- JazzFuzz/DependencyInitialization.cs
+++ JazzFuzz/DependencyInitialization.cs
@@ -11,10 +11,10 @@ namespace JazzFuzz
         public static ServiceProvider SetupDependencies()
         {
             IServiceCollection services = new ServiceCollection();
-            services.AddSingleton<IRangeConfiguration, DescendingRangeConfiguration>();
+            services.AddSingleton<IRangeConfiguration, AscendingRangeConfiguration>();
             services.AddSingleton<IReplacementConfiguration, JazzFuzzReplacementConfiguration>();
             services.AddSingleton<IGeneratorService, GeneratorService>();
-            services.AddSingleton<IPresenterService, ConsolePresenterService>();
+            services.AddSingleton<IPresenterService, MorseBeepPresenterService>();
             return services.BuildServiceProvider();
         }
     }
