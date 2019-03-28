fc8398a78e5df39443729f0ae5181b7a014e832e
Tomas Hensrud Gulla <tomas.h.gulla@novacare.no>
2019-03-28 20:05:53 +0100

    Add new range configuration, that only produce fizz and buzz and fizzbuzz

---
+++ JazzFuzz/RangeConfigurations/FizzBuzzOnlyRangeConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace JazzFuzz.RangeConfigurations
+{
+    public class FizzBuzzOnlyRangeConfiguration : IRangeConfiguration
+    {
+        public IEnumerable<int> GetRange()
+        {
+            return Enumerable.Range(0, 100).Where(x => x % 3 == 0 || x % 5 == 0);
+        }
+    }
+}
